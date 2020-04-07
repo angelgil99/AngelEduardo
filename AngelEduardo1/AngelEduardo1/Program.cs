cdf91e452fe0fab60400d2a5a47fa9c63f1683dd
Angel Eduardo Gil Molina <angel.gil99.ag@gmail.com>
2020-04-07 14:36:33 -0300

    Tareas

---
+++ AngelEduardo1/AngelEduardo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AngelEduardo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
