5a86c711c553e1a90cea1dcb68370c86149dd990
Ulises Estecche <uestecche@yodle.com>
2020-04-14 21:55:45 -0300

    Usando un arreglo (array) y el algoritmo de bubble sort o burbuja para ordernar los numeros y saber cual es el mayor

--- AngelEduardo1/AngelEduardo1/Program.cs
+++ AngelEduardo1/AngelEduardo1/Program.cs
@@ -2,77 +2,45 @@
 
 namespace AngelEduardo1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("- - - -  - - - EJERCICIO 1- - - - - - - - -");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("A continuacion ingrese tres numeros...");
-            Console.ForegroundColor = ConsoleColor.Red; int n1 = Convert.ToInt32(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Ingrese segundo valor: ");
-            Console.ForegroundColor = ConsoleColor.Red; int n2 = Convert.ToInt32(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Ingrese tercer valor: ");
-            Console.ForegroundColor = ConsoleColor.Red; int n3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Presione Enter...");
-            Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"Teniendo estos tres numeros queremos saber cual de ellos es mayor...");
-            Console.ForegroundColor = ConsoleColor.White;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.ForegroundColor = ConsoleColor.Blue;
+		Console.WriteLine("- - - -  - - - EJERCICIO 1- - - - - - - - -");
+		Console.ForegroundColor = ConsoleColor.White;
+		Console.WriteLine("A continuacion ingrese tres numeros...");
+		Console.ForegroundColor = ConsoleColor.Red; int n1 = Convert.ToInt32(Console.ReadLine());
+		Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Ingrese segundo valor: ");
+		Console.ForegroundColor = ConsoleColor.Red; int n2 = Convert.ToInt32(Console.ReadLine());
+		Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Ingrese tercer valor: ");
+		Console.ForegroundColor = ConsoleColor.Red; int n3 = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine();
+		Console.ForegroundColor = ConsoleColor.White;
+		Console.WriteLine("Presione Enter...");
+		Console.ReadLine();
+		Console.ForegroundColor = ConsoleColor.Blue;
+		Console.WriteLine($"Teniendo estos tres numeros queremos saber cual de ellos es mayor...");
+		Console.ForegroundColor = ConsoleColor.White;
 
-            Console.WriteLine(" ");
-            if (n1 > n2 && n1 > n3)
-            {
-                Console.WriteLine($"{n1} Es el numero Mayor...");
-            }
-            else if (n1 < n2 & n1 > n3)
-            {
-                Console.WriteLine($"{n1} Es mayor que {n3} pero no es Mayor que {n2}, Por lo tanto no es el numero Mayor");
-            }
-            else if (n1 > n2 & n1 < n3)
-            {
+		Console.WriteLine(" ");
+    
+		int[] numbers = {n1, n2, n3};
+		int temp;
 
-                Console.WriteLine($"{n1} es mayor que {n2} pero no es mayor que {n3}, Por lo tanto no es el numero Mayor... ");
-            }
+		for (int j = 0; j <= numbers.Length - 2; j++) {
+			for (int i = 0; i <= numbers.Length - 2; i++) {
+				if (arr[i] > arr[i + 1]) {
+					temp= arr[i + 1];
+					arr[i + 1] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		Console.WriteLine($"{numbers[numbers.Length - 1]} Es el numero Mayor...");
 
-
-            Console.WriteLine();
-            if (n2 > n1 & n2 > n3)
-            {
-                Console.WriteLine($"{n2} Es el numero Mayor...");
-            }
-            else if (n2 < n1 & n2 > n3)
-            {
-                Console.WriteLine($"{n2} Es mayor que {n3} pero no es Mayor que {n1}, Por lo tanto no es el numero Mayor");
-            }
-            else if (n2 > n1 & n2 < n3)
-            {
-
-                Console.WriteLine($"{n2} es mayor que {n1} pero no es mayor que {n3}, Por lo tanto no es el numero Mayor... ");
-            }
-            Console.WriteLine();
-            if (n3 > n2 & n3 > n1)
-            {
-                Console.WriteLine($"{n3} Es el numero Mayor...");
-            }
-            else if (n3 < n2 & n3 > n1)
-            {
-                Console.WriteLine($"{n3} Es mayor que {n1} pero no es Mayor que {n2}, Por lo tanto no es el numero Mayor");
-            }
-            else if (n3 > n2 & n3 < n1)
-            {
-
-                Console.WriteLine($"{n3} es mayor que {n2} pero no es mayor que {n1}, Por lo tanto no es el numero Mayor... ");
-            }
-            Console.WriteLine();
-
-
-
-            Console.WriteLine();
-            Console.ReadLine();
-        }
-    }
+		Console.WriteLine();
+		Console.ReadLine();
+	}
+}
 }
